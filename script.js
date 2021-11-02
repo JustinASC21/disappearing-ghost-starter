2155715b294888a3edf68638928ac1401b7d48ff
Glitch (torch-frosted-huckleberry) <none>
2021-11-02 22:27:15 +0000

    🦃🏂 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
+let inputText = $(".input");
 
+// hide ghost
+$(".hide-button").click(function() {
+  $(".ghost-image").hide();
+});
+
+// show ghost
+$(".show-button").click(function() {
+  $(".ghost-image").show();
+});
+// name me 
+$(".name-button").click(function() {
+  $("h1").text("My name is " + inputText.val() + " and I am friendly!");
+})
+// update image
+$(".update-img-button").click(function {
+  $(".ghost-image").attr("src","https://cdn.glitch.me/e305ff80-c6e8-43db-8f58-2459e07c1236%2Fghost.png?v=1635891963908");
+})
+// add messages
+$(".message-button").click( function() {
+  $(".message").append(inputText.val() + "<br>");
+})
