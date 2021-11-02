d1f3610a8bdd21f32c86cb5188c872a2babc9907
Glitch (torch-frosted-huckleberry) <none>
2021-11-02 22:35:39 +0000

    💍🐙 Updated with Glitch

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ $(".name-button").click(function() {
   $("h1").text("My name is " + inputText.val() + " and I am friendly!");
 })
 // update image
-$(".update-img-button").click(function {
+$(".update-img-button").click(function() {
   $(".ghost-image").attr("src","https://cdn.glitch.me/e305ff80-c6e8-43db-8f58-2459e07c1236%2Fghost.png?v=1635891963908");
-})
+});
 // add messages
 $(".message-button").click( function() {
   $(".message").append(inputText.val() + "<br>");
